16ad9ae5485066d45a0fdc3b8195bb43a3f874ad
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2020-12-12 13:47:22 -0600

    first commit

---
+++ notes.js
@@ -0,0 +1,17 @@
+// JavaScript Arrays
+
+var tvShows = [
+  'The Office', 
+  'Stranger Things', 
+  'Parks and Rec'
+];
+
+var userSavedMovies = [];
+
+userSavedMovies.push('Toy Story');
+userSavedMovies.push('The Matrix');
+userSavedMovies.push('Braveheart');
+
+// userSavedMovies.pop(); // remove the last value in an array
+
+console.log(userSavedMovies.length);
